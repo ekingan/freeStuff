de49cc1fb5f00c0ce1bdc2ed519e8e5b8451b363
ekingan <ekingan@gmail.com>
2016-06-27 15:48:47 -0700

    Created custom post cell and test data

---
+++ photoBlog/PostCell.swift
@@ -0,0 +1,33 @@
+//
+//  PostCell.swift
+//  photoBlog
+//
+//  Created by Math LLC on 6/27/16.
+//  Copyright © 2016 Math LLC. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var titleLbl: UILabel!
+    @IBOutlet weak var descLbl: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    func configureCell(post: Post) {
+        titleLbl.text = post.title
+        descLbl.text = post.postDesc
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- photoBlog/ViewController.swift
+++ photoBlog/ViewController.swift
@@ -8,21 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
+    
+    var posts = [Post]()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        var post1 = Post(imagePath: "", title: "Post 1", description: "This is post one")
+        var post2 = Post(imagePath: "", title: "Post 2", description: "This is post two")
+        var post3 = Post(imagePath: "", title: "POst 3", description: "This is post three")
+        
+        posts.append(post1)
+        posts.append(post2)
+        posts.append(post3)
+        
+        tableView.reloadData()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+        
     }
-
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let post = posts[indexPath.row]
+        
+        if let cell = tableView.dequeueReusableCellWithIdentifier("PostCell") as? PostCell {
+        
+        cell.configureCell(post)
+        return cell
+        } else {
+            let cell = PostCell()
+            cell.configureCell(post)
+            return cell
+        }
+        
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 81.0
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+//    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+//        <#code#>
+//    }
 
 }
 
